a3a09fd26e870e4fa681e8f17c586573b21483e2
abhishek <abhishakem@hotmail.com>
2024-11-22 16:53:09 +0530

    Javascript added to the repo and added Mouse events

---
+++ script.js
@@ -0,0 +1,22 @@
+let formSubm = document.querySelector("#submitBtn");
+
+formSubm.addEventListener("click", SubmitEventFunc, false);
+
+let counter =0;
+function SubmitEventFunc(event) {
+    counter++;
+   document.getElementById("formStatus").innerText= "Form Submitted Without refresh " +counter + " times";
+  event.preventDefault();
+ 
+
+}
+
+formSubm.addEventListener('mouseover', function(event) {
+    document.getElementById("userMouseStatus").innerText="Mouse pointer is hovered on the submit button";
+    console.log('Mouse over event triggered!');
+});
+
+formSubm.addEventListener('mouseout', function(event) {
+    document.getElementById("userMouseStatus").innerText="Mouse pointer is away from the submit button";
+    console.log('Mouse over event triggered!');
+});
