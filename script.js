d6cf6002656a5e25f60459604ce04073906b6f40
abhishek <abhishakem@hotmail.com>
2024-11-22 18:09:00 +0530

    Added Event delegates with multiple buttons to script and HTML

--- script.js
+++ script.js
@@ -2,38 +2,47 @@ let formSubm = document.querySelector("#submitBtn");
 
 formSubm.addEventListener("click", SubmitEventFunc, false);
 
-let counter =0;
+let counter = 0;
 function SubmitEventFunc(event) {
     counter++;
-   document.getElementById("formStatus").innerText= "Form Submitted Without refresh " +counter + " times";
-  event.preventDefault();
- 
+    document.getElementById("formStatus").innerText = "Form Submitted Without refresh " + counter + " times";
+    event.preventDefault();
+
 
 }
 
-formSubm.addEventListener('mouseover', function(event) {
-    document.getElementById("userMouseStatus").innerText="Mouse pointer is hovered on the submit button";
+formSubm.addEventListener('mouseover', function (event) {
+    document.getElementById("userMouseStatus").innerText = "Mouse pointer is hovered on the submit button";
     console.log('Mouse over event triggered!');
 });
 
-formSubm.addEventListener('mouseout', function(event) {
-    document.getElementById("userMouseStatus").innerText="Mouse pointer is away from the submit button";
+formSubm.addEventListener('mouseout', function (event) {
+    document.getElementById("userMouseStatus").innerText = "Mouse pointer is away from the submit button";
     console.log('Mouse over event triggered!');
 });
 
 
-document.addEventListener('keydown', function(event)  {
-     document.getElementById('keyStat').textContent = `Last key pressed : ${event.key}`;
+document.addEventListener('keydown', function (event) {
+    document.getElementById('keyStat').textContent = `Last key pressed : ${event.key}`;
 });
 
 
-let formInputs =document.getElementsByClassName("form-inputs");
+let formInputs = document.getElementsByClassName("form-inputs");
 
 
 for (let index = 0; index < formInputs.length; index++) {
-    formInputs[index].addEventListener('focus', function(event){
-        document.getElementById('focusStat').innerHTML =`Focussed on the <b> ${formInputs[index].attributes['data-id'].value} </b> field from the form`
+    formInputs[index].addEventListener('focus', function (event) {
+        document.getElementById('focusStat').innerHTML = `Focussed on the <b> ${formInputs[index].attributes['data-id'].value} </b> field from the form`
     })
-    
+    formInputs[index].addEventListener('focusout', function (event) {
+        document.getElementById('focusStat').innerHTML = `Focussed out the <b> ${formInputs[index].attributes['data-id'].value} </b> field from the form`
+    })
+
 }
- 
+
+
+document.getElementById('buttonContainer').addEventListener('click', function (event) {
+    let clickedElement = event.target; // Check if a button was clicked 
+    if (clickedElement.tagName === 'BUTTON') { const message = clickedElement.getAttribute('data-cont'); document.getElementById('BtnMessage').textContent = message; }
+});
+
